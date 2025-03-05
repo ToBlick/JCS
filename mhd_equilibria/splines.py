c983ec46863956db96c04e3211526e60ca0b8ef2
Tobias Blickhan <tobias.blickhan@gmail.com>
2025-03-04 23:07:56 -0600

    remove unneccessary argument from spline function

--- mhd_equilibria/splines.py
+++ mhd_equilibria/splines.py
@@ -14,13 +14,13 @@ __all__ = [
 # TODO: Splines right now only work on [0, 1]
 # TODO: NaN bug when differentiating splines of order 4 or higher
 
-def indicator(x, i, T, p, n, m):
+def indicator(x, i, T, p, n):
     return jnp.where(jnp.logical_and(T[i] <= x, x < T[i+1]), 
         1.0, 
         0.0
         )
     
-def indicator_clamped(x, i, T, p, n, m):
+def indicator_clamped(x, i, T, p, n):
     return jnp.where(jnp.logical_and(T[i] <= x, x < T[i+1]), 
         1.0, 
         jnp.where(jnp.logical_and(x == T[i+1], i == n-1),
@@ -37,18 +37,18 @@ def safe_divide(numerator, denominator):
 
 #TODO: the value of eps maybe should be a parameter
 # @partial(jax.jit, static_argnums=(3,6))
-def spline(x, i, T, p, n, m, type):
+def spline(x, i, T, p, n, type):
     eps = 1e-16
     if p == 0:
         if type == 'clamped':
-            return indicator_clamped(x, i, T, p, n, m)
+            return indicator_clamped(x, i, T, p, n)
         else:
-            return indicator(x, i, T, p, n, m)
+            return indicator(x, i, T, p, n)
     else:
         w_1 = safe_divide(x - T[i] + eps,T[i + p] - T[i] + eps)
         w_2 = safe_divide(T[i + p + 1] - x + eps, T[i + p + 1] - T[i + 1] + eps)
-        return w_1 * spline(x, i, T, p - 1, n, m, type) \
-            + w_2 * spline(x, i + 1, T, p - 1, n, m, type)
+        return w_1 * spline(x, i, T, p - 1, n, type) \
+            + w_2 * spline(x, i + 1, T, p - 1, n, type)
             
 def knot_vector(n, p, type='open'):
     if type == 'periodic':
@@ -67,20 +67,18 @@ def knot_vector(n, p, type='open'):
 def get_spline(n, p, type='clamped'):
     if type == 'periodic':
         n = n + p
-        m = n - p + 1
         T = knot_vector(n, p, type)
         def _spline(x, i):
             i = i + p
             return jax.lax.cond(i > n - 2*p,
-                lambda _: spline(x, i, T, p, n, m, 'periodic') \
-                        + spline(x, i - n + p, T, p, n, m, 'periodic'),
-                lambda _: spline(x, i, T, p, n, m, 'periodic'),
+                lambda _: spline(x, i, T, p, n, 'periodic') \
+                        + spline(x, i - n + p, T, p, n, 'periodic'),
+                lambda _: spline(x, i, T, p, n, 'periodic'),
                 operand=None)
     elif type == 'clamped':
         T = knot_vector(n, p, type)
-        m = n - p + 1
         def _spline(x, i):
-            return spline(x, i, T, p, n, m, 'clamped')
+            return spline(x, i, T, p, n, 'clamped')
     return _spline
 
 
